c2ef9a98bfd4b2f8dfc121aa655644c604494ec7
Justinbarry188 <79929356+Justinbarry188@users.noreply.github.com>
2021-04-01 08:41:12 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
+// Dose the same as button B
 input.onButtonPressed(Button.A, function () {
     num1 += 1
     basic.showNumber(num1)
 })
+// When you press this it will take the two stored numbers and put them in reverse 
 input.onButtonPressed(Button.AB, function () {
     basic.showNumber(num2)
     basic.showNumber(num1)
 })
+// when you press button B it will store the number you choose and save it for later 
 input.onButtonPressed(Button.B, function () {
     num2 += 1
     basic.showNumber(num2)
